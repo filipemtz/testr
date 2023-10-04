838a30f2b62170be44ca12aef0806cfe6c7d7b1d
filipemtz <filipemtz@gmail.com>
2023-10-04 14:20:58 -0300

    show deadline in question and change in the timezone

--- global/settings.py
+++ global/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
